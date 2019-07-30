64f151b2f01c8793b6eca53b791bb421b1ad396d
Peter Lisy <pelisy@microsoft.com>
2019-07-30 12:37:57 -0700

    Added autosuggestions plugin.

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-[ -d "~/.oh-my-zsh" ] && ln -sf .zshrc ~
+# Go and override any .zshrc with my own!
+[ -d "~/.zshrc" ] && ln -sf .zshrc ~
+
 [ -d "~/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+[ -d "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
