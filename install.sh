f06f13d9f36166d45f1a4711d0a4cb439205b43f
Peter Lisy <pelisy@microsoft.com>
2019-07-30 14:46:18 -0700

    Removed the source

--- install.sh
+++ install.sh
@@ -6,4 +6,3 @@ ln -sf .zshrc ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-source ~/.zshrc
