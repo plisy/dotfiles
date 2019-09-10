ce9ad3f2fc4c37068081491388a3ce9d1c868510
Peter Lisy <pelisy@microsoft.com>
2019-09-10 12:15:43 -0700

    fixing up install script

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 DOTFILES="$(pwd)"
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sh -c "$(curl --proto '=https' --tlsv1.2 -sSfL https://sh.rustup.rs)" -- -y
+source $HOME/.cargo/env
+
+cargo install starship
+
+npm config set prefix '~/.npm-global'
+
 # Go and override any .zshrc with my own!
 cd $HOME
 
@@ -9,12 +19,3 @@ ln -sf $DOTFILES/_zshrc .zshrc
 ln -sf $DOTFILES/_vimrc .vimrc
 
 cd $DOTFILES
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-sh -c "$(curl --proto '=https' --tlsv1.2 -sSfL https://sh.rustup.rs)" -- -y
-
-cargo install starship
-
-npm config set prefix '~/.npm-global'
