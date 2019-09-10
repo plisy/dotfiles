cdd4e501a4682bc75aad522e4126e2d14020aa6c
Peter Lisy <pelisy@microsoft.com>
2019-09-10 19:28:01 +0000

    Finally works the way I want it to 🍻

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 DOTFILES="$(pwd)"
 
+# Create locals override file so my .zshrc doesn't complain
+touch ~/.locals
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/pkulev/zsh-rustup-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/rustup
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSfL https://sh.rustup.rs)" -- -y
@@ -10,11 +14,13 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 cargo install starship
 
+echo "Setting npm prefix to $HOME/.npm-global"
 npm config set prefix '~/.npm-global'
 
 # Go and override any .zshrc with my own!
 cd $HOME
 
+echo "Linking config files"
 ln -sf $DOTFILES/_zshrc .zshrc
 ln -sf $DOTFILES/_vimrc .vimrc
 
