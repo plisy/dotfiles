b14d49a7186144aba4fdb5312fec329f3ff3a516
Peter Lisy <pelisy@microsoft.com>
2019-09-10 12:10:15 -0700

    More changes.

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+DOTFILES="$(pwd)"
+
 # Go and override any .zshrc with my own!
-ln -sf .zshrc ~
+cd $HOME
+
+ln -sf $DOTFILES/_zshrc .zshrc
+ln -sf $DOTFILES/_vimrc .vimrc
+
+cd $DOTFILES
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+sh -c "$(curl --proto '=https' --tlsv1.2 -sSfL https://sh.rustup.rs)" -- -y
+
+cargo install starship
+
+npm config set prefix '~/.npm-global'
