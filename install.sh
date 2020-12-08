ed10766bf85dd73d01edad533f24c41bd75767ee
Peter Lisy <pelisy@microsoft.com>
2020-12-08 00:48:36 +0000

    Removed actual content.

--- install.sh
+++ install.sh
@@ -4,32 +4,34 @@ DOTFILES="$( cd "$(dirname "$0")" ; pwd -P )"
 
 echo "Context: DOTFILES=${DOTFILES}"
 
-# Create locals override file so my .zshrc doesn't complain
-touch ~/.locals
+echo "Dummy dotfiles"
 
-sh $DOTFILES/install-zsh.sh
+# # Create locals override file so my .zshrc doesn't complain
+# touch ~/.locals
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/pkulev/zsh-rustup-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/rustup
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# sh $DOTFILES/install-zsh.sh
 
-echo "Setting npm prefix to $HOME/.npm-global"
-npm config set prefix '~/.npm-global'
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+# git clone https://github.com/pkulev/zsh-rustup-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/rustup
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "Setting up git preferences"
-sh $DOTFILES/configure-git.sh
+# echo "Setting npm prefix to $HOME/.npm-global"
+# npm config set prefix '~/.npm-global'
 
-# Go and override any .zshrc with my own!
-cd $HOME
+# echo "Setting up git preferences"
+# sh $DOTFILES/configure-git.sh
 
-echo "Linking config files"
-ln -sf $DOTFILES/_zshrc .zshrc
-ln -sf $DOTFILES/_vimrc .vimrc
-ln -sf $DOTFILES/_cloudenv-settings.json .cloudenv-settings.json
+# # Go and override any .zshrc with my own!
+# cd $HOME
 
-cd $DOTFILES
+# echo "Linking config files"
+# ln -sf $DOTFILES/_zshrc .zshrc
+# ln -sf $DOTFILES/_vimrc .vimrc
+# ln -sf $DOTFILES/_cloudenv-settings.json .cloudenv-settings.json
 
-sh -c "$(curl --proto '=https' --tlsv1.2 -sSfL https://sh.rustup.rs)" -- -y
-export PATH="$HOME/.cargo/bin:$PATH"
+# cd $DOTFILES
 
-cargo install starship
+# sh -c "$(curl --proto '=https' --tlsv1.2 -sSfL https://sh.rustup.rs)" -- -y
+# export PATH="$HOME/.cargo/bin:$PATH"
+
+# cargo install starship
